9391f17771c316e361a1f35d8db1ccb6f31fe5db
edu15076 <eduardbh2014@gmail.com>
2023-03-11 15:41:24 -0300

    Alterações no models

--- music_project/music/models.py
+++ music_project/music/models.py
@@ -5,6 +5,18 @@ from django.db import models
 # Create your models here.
 
 
+class Gender(models.Model):
+    MALE = 'M'
+    FEMALE = 'F'
+    OTHER = 'O'
+
+    GENDER_CHOICES = (
+        (MALE, 'Man'),
+        (FEMALE, 'Woman'),
+        (OTHER, 'Other')
+    )
+
+
 class Banda(models.Model):
     nome = models.CharField(max_length=100)
     data_de_criacao = models.DateField()
@@ -19,7 +31,7 @@ class Banda(models.Model):
 
 class Musico(models.Model):
     nome = models.CharField(max_length=100)
-    genero = models.CharField(max_length=10)
+    genero = models.CharField(max_length=1, choices=Gender.GENDER_CHOICES)
 
     def __str__(self):
         return self.nome
